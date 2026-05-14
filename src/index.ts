6b7b192d0498a9a192666fbdd590acd20bb2e0ab
To Uyen <duongbangha19102004@gmail.com>
2026-05-15 01:37:12 +0700

    chore: setup express server and mysql connection

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import { checkDatabaseConnection } from "./config/database";
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Chào mừng đến với Floria API Backend!");
+});
+
+const startServer = async () => {
+  await checkDatabaseConnection();
+
+  app.listen(PORT, () => {
+    console.log(`Server đang chạy tại: http://localhost:${PORT}`);
+  });
+};
+
+startServer();
